5172874204ffef466fcdddafd271c2f19953790f
HOME <HOME@54VOQUOCAN>
2026-05-15 14:40:57 +0700

    Add project files.

---
+++ CSLT/Exercise 2/bai19.cs
@@ -0,0 +1,23 @@
+﻿using System;
+namespace bai19
+{
+    class Program 
+    {
+        static void Main19(string[] args)
+        {
+            Console.WriteLine("nhap so ");
+            int a = Convert.ToInt32(Console.ReadLine());
+            int sum = 0;
+            while (a != 0)
+            {
+                int b = a % 10;
+                sum += b;
+                a /= 10;
+
+            }
+            Console.WriteLine("ket qua:" + sum);
+        }
+    }
+}
+
+
